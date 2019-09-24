aff63db5144b1661e9e8c84585c30b7d9eeacc97
joshtru <joshuaojc@gmail.com>
2019-09-24 17:45:43 -0500

    Updates

---
+++ precache-manifest.088905ee5a824d7442990aeba7f0c49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979942f278ff8bcec3345e830b4705aa",
+    "url": "/joshtru/monsters-page/index.html"
+  },
+  {
+    "revision": "b06c18e2f4f21b0bd6ec",
+    "url": "/joshtru/monsters-page/static/css/main.cc5bc24a.chunk.css"
+  },
+  {
+    "revision": "d13a353a0fe4879286ec",
+    "url": "/joshtru/monsters-page/static/js/2.d395d0d5.chunk.js"
+  },
+  {
+    "revision": "b06c18e2f4f21b0bd6ec",
+    "url": "/joshtru/monsters-page/static/js/main.d978950d.chunk.js"
+  },
+  {
+    "revision": "75d8bd655b52cdc59fdc",
+    "url": "/joshtru/monsters-page/static/js/runtime-main.df813030.js"
+  }
+]);
