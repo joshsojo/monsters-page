41429712fdd8e3ee795bbff18fa293da211185c4
joshtru <joshuaojc@gmail.com>
2019-09-25 04:16:32 -0500

    Added heading to the page, with styles to the background

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monster Pages</h1>
         <SearchBox
           placeholder={"Search monsters"}
           handleChange={this.handleChange}
